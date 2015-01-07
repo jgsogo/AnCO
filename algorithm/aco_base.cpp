1b9efeeb144bf318c8af2a195e8e5a77984002fb
Javier G. Sogo <jgsogo+github@gmail.com>
2015-01-07 10:48:18 +0100

    La ejecución del algoritmo se separa en funciones

--- algorithm/aco_base.cpp
+++ algorithm/aco_base.cpp
@@ -1,6 +1,5 @@
 
 #include "aco_base.h"
-#include <set>
 #include "../utils/random.h"
 
 
@@ -13,11 +12,38 @@ namespace AnCO {
             return pow(ptr->data.pheromone[id], alpha)* pow(1/ptr->data.distance, beta);
             }
 
-        int aco_base::get_feasible_edges(graph& graph, const graph::_t_node_id& node, std::vector<edge_ptr>& feasible_edges) {
+        int aco_base::get_feasible_edges(graph& graph, const graph::_t_node_id& node, std::vector<edge_ptr>& feasible_edges, const std::set<graph::_t_node_id>& visited) {
             std::pair<std::vector<edge_ptr>, std::vector<edge_ptr>> edges_pair;
             graph.get_edges(node, edges_pair);
             feasible_edges = edges_pair.first;
+
+            // Delete already visited ones
+            feasible_edges.erase(std::remove_if(feasible_edges.begin(), feasible_edges.end(), [&visited](edge_ptr ptr) -> bool {
+                return !(visited.find(ptr->end) == visited.end());
+                }), feasible_edges.end());
+
             return feasible_edges.size();
+            }
+
+        edge_ptr aco_base::select_edge(const std::vector<edge_ptr>& feasible_edges, const unsigned int& pherom_id) {
+            float max_pheromone = 0.f;
+            std::for_each(feasible_edges.begin(), feasible_edges.end(), [&pherom_id, &max_pheromone](edge_ptr ptr) {
+                max_pheromone += aco_base::prob_edge(ptr, pherom_id);
+                });
+            // Elegir uno de los edges
+            float r = utils::random::get_random(max_pheromone);
+            std::vector<edge_ptr>::const_iterator it = feasible_edges.begin();
+            while ((it != feasible_edges.end()) && (r > (*it)->data.pheromone[pherom_id])) {
+                r -= aco_base::prob_edge((*it), pherom_id);
+                ++it;
+                }
+
+            if (it!=feasible_edges.end()) {
+                return (*it);
+                }
+            else {
+                return (*feasible_edges.rbegin());
+                }
             }
 
         bool aco_base::run(graph& graph, const graph::_t_node_id& node, const unsigned int& pherom_id, _f_success& suc, std::vector<edge_ptr>& _path, const int& max_steps) {
@@ -29,36 +55,14 @@ namespace AnCO {
             do {
                 // 1) Calcular los edges que son posibles
                 std::vector<edge_ptr> feasible_edges;
-                aco_base::get_feasible_edges(graph, current_node, feasible_edges);
-
-                // 2) Suma de concentraciones de feromona
-                float max_pheromone = 0.f;
-                feasible_edges.erase(std::remove_if(feasible_edges.begin(), feasible_edges.end(), [&pherom_id, &step, &visited, &max_pheromone](edge_ptr ptr) -> bool {
-                    if(visited.find(ptr->end) == visited.end()) {
-                        max_pheromone += aco_base::prob_edge(ptr, pherom_id);
-                        return false;
-                        }
-                    return true;
-                    }), feasible_edges.end());
-
-                if (feasible_edges.size() == 0) {
+                int n = aco_base::get_feasible_edges(graph, current_node, feasible_edges, visited);
+                if (n == 0) {
                     return false; // break. No more nodes to visit.
                     }
 
-                // 2) Elegir uno de los edges
-                float r = utils::random::get_random(max_pheromone);
-                std::vector<edge_ptr>::iterator it = feasible_edges.begin();
-                while ((it != feasible_edges.end()) && (r > (*it)->data.pheromone[pherom_id])) {
-                    r -= aco_base::prob_edge((*it), pherom_id);
-                    ++it;
-                    }
-
-                if (it!=feasible_edges.end()) {
-                    edge = *it;
-                    }
-                else {
-                    edge = *feasible_edges.rbegin();
-                    }
+                // 2) Elegir uno
+                edge_ptr edge = aco_base::select_edge(feasible_edges, pherom_id);
+                
                 // 3) Añadir al path y actualizar variables.
                 _path.push_back(edge);
                 current_node = edge->end;
--- algorithm/aco_base.h
+++ algorithm/aco_base.h
@@ -1,6 +1,8 @@
 
 
 #include <functional>
+#include <vector>
+#include <set>
 
 #include "../graph/graph.h"
 
@@ -17,15 +19,16 @@ namespace AnCO {
                 static float prob_edge(const edge_ptr& ptr, const unsigned int& id);
 
                 // Ejecución del algoritmo
-                static bool run(    /*const*/ graph& graph,         // grafo en el que me muevo
-                                    const graph::_t_node_id& node,  // nodo inicial
-                                    const unsigned int& pherom_id,  // id feromona
-                                    _f_success& suc,                // función para determinar si he encontrado el destino
-                                    std::vector<edge_ptr>& _path,   // camino seguido
-                                    const int& max_steps = 100);    // número máximo de pasos
+                static bool run(    /*const*/ graph& graph,         // [in] grafo en el que me muevo
+                                    const graph::_t_node_id& node,  // [in] nodo inicial
+                                    const unsigned int& pherom_id,  // [in] id feromona
+                                    _f_success& suc,                // [in] función para determinar si he encontrado el destino
+                                    std::vector<edge_ptr>& _path,   // [out] camino seguido
+                                    const int& max_steps = 100);    // [in] número máximo de pasos
 
             protected:
-                static int get_feasible_edges(graph& graph, const graph::_t_node_id& node, std::vector<edge_ptr>& feasible_edges);
+                static int get_feasible_edges(graph& graph, const graph::_t_node_id& node, std::vector<edge_ptr>& feasible_edges, const std::set<graph::_t_node_id>& visited);
+                static edge_ptr select_edge(const std::vector<edge_ptr>& feasible_edges, const unsigned int& pherom_id);
 
             public:
                 static float alpha;
