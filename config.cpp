bbb8a2df4df484b1044e16763874a3d38463b591
Javier G. Sogo <jgsogo@gmail.com>
2015-01-17 00:35:20 +0100

    lectura del archivo de configuración (admite comentarios)

--- config.cpp
+++ config.cpp
@@ -1,16 +1,37 @@
 
 #include "config.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
 //#include "david/yaml-cpp/include/yaml-cpp/yaml.h"
 
 namespace AnCO {
 
     config load_config(const std::string& file) {
         config hret;
-        hret.dataset = file;
-        hret.n_colonies = 10;
-        hret.n_ants_per_colony = 10;
-        hret.max_steps = 5;
+        std::cout << "Loading config from file '" << file << "'" << std::endl;
+
+        //#include <stdio.h>
+        std::ifstream infile(file);
+        std::string line;
+        std::string n1, equal, n2;
+        
+        while(std::getline(infile, line)) {
+            std::istringstream ss(line);
+            if (ss >> n1) {
+                if (n1.compare(0, 1, "#")==0) {
+                    std::cout << "\n\t" << line << std::endl;
+                    continue;
+                    }
+                ss >> equal >> n2;
+                std::cout << "\t" << n1 << equal << "'" << n2 << "'" << std::endl;
+                if (n1.compare("dataset")==0) { hret.dataset = n2;}
+                if (n1.compare("n_colonies")==0) { hret.n_colonies = std::stoi(n2);}
+                if (n1.compare("n_ants_per_colony")==0) { hret.n_ants_per_colony = std::stoi(n2);}
+                if (n1.compare("max_steps")==0) { hret.max_steps = std::stoi(n2);}
+                }
+            }
+        std::cout << std::endl;
         return hret;
         }
-
     }
