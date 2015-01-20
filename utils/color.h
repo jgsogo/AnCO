0ea2c1edcccfedde81c16af885ac5bf271ddd07b
Javier G. Sogo <jgsogo@gmail.com>
2015-01-20 13:13:47 +0100

    colores en windows

--- utils/color.h
+++ utils/color.h
@@ -3,18 +3,30 @@
 
 #include <iostream>
 
+#ifdef _WINDOWS
+#include <windows.h>
+#endif
+
 namespace AnCO {
     namespace utils {
         struct color {
-            static const unsigned DEFAULT = 0;
-            static const unsigned RED = 31;
-            static const unsigned GREEN = 32;
-            static const unsigned BLUE = 34;
-            /*
-            static const std::string BG_RED = "41";
-            static const std::string BG_GREEN = "42";
-            static const std::string BG_BLUE = "44";
-            */
+            #ifdef _WINDOWS
+                static const unsigned int DEFAULT = 7;
+                static const unsigned int RED = FOREGROUND_RED;
+                static const unsigned int GREEN = FOREGROUND_GREEN;
+                static const unsigned BLUE = FOREGROUND_BLUE;
+            #else
+                static const unsigned DEFAULT = 0;
+                static const unsigned RED = 31;
+                static const unsigned GREEN = 32;
+                static const unsigned BLUE = 34;
+                /*
+                static const std::string BG_RED = "41";
+                static const std::string BG_GREEN = "42";
+                static const std::string BG_BLUE = "44";
+                */
+            #endif
+
             color(unsigned c, unsigned def = DEFAULT) : color_default(std::move(def)) {
                 set_color(c);
             };
@@ -22,8 +34,10 @@ namespace AnCO {
                 set_color(color_default);
             };
             static void set_color(unsigned c) {
-                #ifndef WINDOWS
+                #ifndef _WINDOWS
                     std::cout << "\033[" << c << "m";
+                #else
+                    SetConsoleTextAttribute( GetStdHandle( STD_OUTPUT_HANDLE ), c );
                 #endif
             };
             unsigned color_default;
